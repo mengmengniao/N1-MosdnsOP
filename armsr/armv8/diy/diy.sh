a216c6e26177dd6a932796207e7cc0d99cf9e95f
mengmengniao <731729875@qq.com>
2025-12-08 02:17:50 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -12,8 +12,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
