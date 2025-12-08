b6f4fc27ddf46857ab36fe62a1b6696191d5cffd
mengmengniao <731729875@qq.com>
2025-12-08 17:40:17 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,22 +3,30 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Add packages
+# getready
+rm -rf clone
+mkdir -p clone
+
+# clone 
+git clone https://github.com/nantayo/My-Pkg clone/my-pkg
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 --single-branch --depth=1 clone/mosdns
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x --single-branch --depth=1 clone/golang
+git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
-
-#  MosDNS 
-rm -rf feeds/luci/applications/luci-app-mosdns
-git clone https://github.com/sbwml/luci-app-mosdns.git /tmp/sbwml_mosdns
-cp -r /tmp/sbwml_mosdns/luci-app-mosdns package/luci-app-mosdns
-git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-rm -rf /tmp/sbwml_mosdns
+# rm old packages
+rm -rf feeds/luci/applications/luci-app-passwall 
+rm -rf feeds/packages/lang/golang 
+rm -rf feeds/packages/net/mosdns 
+rm -rf feeds/packages/net/v2ray-geodata
 
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/mosdns/luci-app-mosdns clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/golang feeds/packages/lang/
+cp -rf clone/mosdns/mosdns clone/mosdns/v2dat clone/my-pkg/haproxy clone/v2ray-geodata feeds/packages/net/
+cp -rf clone/my-pkg/luci-app-mosdns clone/my-pkg/luci-app-passwall feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
