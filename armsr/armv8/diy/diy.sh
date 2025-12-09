b9c52977bd12b962732524a7c33bb6a24d4db49c
mengmengniao <731729875@qq.com>
2025-12-09 16:51:39 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -8,6 +8,8 @@ rm -rf clone
 mkdir -p clone
 
 # clone 
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/nantayo/My-Pkg clone/my-pkg
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 --single-branch --depth=1 clone/mosdns
@@ -16,6 +18,8 @@ git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
 # rm old packages
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-passwall 
 rm -rf feeds/packages/lang/golang 
 rm -rf feeds/packages/net/mosdns 
